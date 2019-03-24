47ee60c3c2558f8033170ed5bd9751b2f884b429
beatsky <631089634@qq.com>
2019-03-24 13:46:43 +0800

    updata socket

--- public/js/control.js
+++ public/js/control.js
@@ -12,19 +12,28 @@ function speedCalc(sp){
 }
 
 
+function lockBig() {
+	socket.emit('speed', `CD0\/r\/n`);
+}
+
+function lockSmall() {
+	socket.emit('speed', `CD1\/r\/n`);
+}
 
 function addSpeed() {
 	sp++
-  socket.emit('speed', `CE${speedCalc(sp)}\/r\/n`);
+  	socket.emit('speed', `CE${speedCalc(sp)}\/r\/n`);
 	console.log(`CE${speedCalc(sp)}\/r\/n`)
+	speedNum.innerHTML = sp
 }
 
 
 function reduceSpeed() {
-	if(sp==1){
+	if(sp==0){
 		return
 	}
 	sp--
 	socket.emit('speed', `CE${speedCalc(sp)}\/r\/n`);
 	console.log(`CE${speedCalc(sp)}\/r\/n`)
+	speedNum.innerHTML = sp
 }
